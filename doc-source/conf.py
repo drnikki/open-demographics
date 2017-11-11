88fe127ba6d6f35081b367893b111a88631eb3e8
Nikki Stevens <nikkilstevens@gmail.com>
2017-11-10 21:29:11 -0700

    Style fixes

--- doc-source/conf.py
+++ doc-source/conf.py
@@ -93,12 +93,15 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'github_user': 'drnikki',
+    'github_repo': 'open-demographics',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static/custom.css']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
